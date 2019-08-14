788ee8e43cf4c1ec73d750819edb416755175180
TaeJoonJeon <jeon3029@naver.com>
2019-08-14 16:12:41 +0900

    first commit

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y;
+  
+  cout << "Please enter two numbers : ";
+  cin>>x>>y;
+  int sum= x+y;
+  cout<<"sum is "<<sum<<" "<<endl;
+  return 0;
+}
