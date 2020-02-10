9d32738b005bd8c5a51a56ee697372cd6d0ac01d
jeon3029 <jeon3029@naver.com>
2020-02-10 16:33:22 +0900

    delete unnecessary code

--- swexpert/dynamic/3752_possible_point/3752.cpp
+++ swexpert/dynamic/3752_possible_point/3752.cpp
@@ -1,9 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int N;
-vector<int> point(100,0);
 bool check[100*100+1];
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
@@ -14,21 +12,18 @@ int main(){
         answer.clear();
         memset(check,0,sizeof(check));
         answer.push_back(0);
+        int point;
         for(int i=0;i<N;i++){
-            cin>>point[i];
-            vector<int> temp;
-            for(int j=0;j<answer.size();j++){
-                int number = answer[j] + point[i];
+            cin>>point;
+            int len = answer.size();
+            for(int j=0;j<len;j++){
+                int number = answer[j] + point;
                 if(!check[number]){
                     check[number]=1;
-                    temp.push_back(number);
+                    answer.push_back(number);
                 }
             }
-            answer.insert(answer.end(),temp.begin(),temp.end());
-        }  
-        // for(int i=0;i<1000;i++){
-        //     if(check[i])cout<<i<<" ";
-        // }cout<<"\n";
+        }
         cout<<"#"<<t_case<<" "<<answer.size()<<"\n";
     }
 }
