c0d65cf2cbaf7be4481b28aff4eed4388e9ef45d
jeon3029 <jeon3029@naver.com>
2020-02-13 15:00:04 +0900

    some grammartical typo

--- Organize_Study/test/find.sh
+++ Organize_Study/test/find.sh
@@ -3,6 +3,6 @@ for((i=1;;++i)); do
     ./gen $i > int
     #./correct < int > out1
     #./brute < int > out2
-    diff -w out1 out2 || break
-    #diff -w (./brute < int) (./correct < int ) ||break
+    #diff -w out1 out2 || break
+    diff -w <(./brute < int) <(./correct < int)  ||break
 done
