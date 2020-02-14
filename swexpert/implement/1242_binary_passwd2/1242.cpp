40fc418891588d03f65230256c641a17c2a2b9e2
jeon3029 <jeon3029@naver.com>
2020-02-14 17:30:02 +0900

    save temporary state

--- swexpert/implement/1242_binary_passwd2/1242.cpp
+++ swexpert/implement/1242_binary_passwd2/1242.cpp
@@ -1,10 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> pi;
-
+typedef pair<bool, int> pi;
 bool Map[2000][2000];
-vector<string> sMap;
-vector<vector<bool>> pat;
+vector<vector<int>> rat;//ratio
+vector<vector<pi>> count_num;
 int N,M;
 int NN;
 bool Hex[16][4] = {
@@ -26,70 +25,78 @@ bool Hex[16][4] = {
 {1,1,1,1}
 };
 void init(){
-    pat.emplace_back(vector<bool>{0,0,0,1,1,0,1});//0
-    pat.emplace_back(vector<bool>{0,0,1,1,0,0,1});//1
-    pat.emplace_back(vector<bool>{0,0,1,0,0,1,1});//2
-    pat.emplace_back(vector<bool>{0,1,1,1,1,0,1});//3
-    pat.emplace_back(vector<bool>{0,1,0,0,0,1,1});//4
-    pat.emplace_back(vector<bool>{0,1,1,0,0,0,1});//5
-    pat.emplace_back(vector<bool>{0,1,0,1,1,1,1});//6
-    pat.emplace_back(vector<bool>{0,1,1,1,0,1,1});//7
-    pat.emplace_back(vector<bool>{0,1,1,0,1,1,1});//8
-    pat.emplace_back(vector<bool>{0,0,0,1,0,1,1});//9
+    rat.emplace_back(vector<int>{3,2,1,1});
+    rat.emplace_back(vector<int>{2,2,2,1});
+    rat.emplace_back(vector<int>{2,1,2,2});
+    rat.emplace_back(vector<int>{1,4,1,1});
+    rat.emplace_back(vector<int>{1,1,3,2});
+    rat.emplace_back(vector<int>{1,2,3,1});
+    rat.emplace_back(vector<int>{1,1,1,4});
+    rat.emplace_back(vector<int>{1,3,1,2});
+    rat.emplace_back(vector<int>{1,2,1,3});
+    rat.emplace_back(vector<int>{3,1,1,2});
 }
 void input(){
     cin>>N>>M;
-    NN=0;
     int check=0;
+    count_num.clear();
     for(int i=0;i<N;i++){
         string s;cin>>s;
-        sMap.push_back(s);
         int pos;
-        int flag=0;
-        for(int j=0;j<M;j++){
-            if(s[j]!='0'){
-                flag=1;break;
-            }
-        }
-        if(flag==0 && check!=1)continue;
-        check=1;
-        if(flag==0)check=0;
         for(int j=0;j<M;j++){
             if(s[j]>='0' && s[j]<='9')pos = s[j]-'0';
             else pos = s[j]-'A'+10;
-            Map[NN][j*4] = Hex[pos][0];
-            Map[NN][j*4+1] = Hex[pos][1];
-            Map[NN][j*4+2] = Hex[pos][2];
-            Map[NN][j*4+3] = Hex[pos][3];
+            Map[i][j*4] = Hex[pos][0];
+            Map[i][j*4+1] = Hex[pos][1];
+            Map[i][j*4+2] = Hex[pos][2];
+            Map[i][j*4+3] = Hex[pos][3];
         }
-        NN++;
+        vector<pi> tt(M*4,{0,0});
+        count_num.push_back(tt);
     }
-}
-int calc(const int x,const int y,const int multi){
-    if(Map[x][y]==1)return -1;//패턴은 항상 0으로 시작
-    vector<int> match(10,1);
-    int cnt_match=10;
-    for(int i=0;i<7;i++){
-        for(int j=0;j<10;j++){
-            if(cnt_match==0)break;
-            if(match[j]){
-                for(int k=0;k<multi;k++){
-                    if(Map[x][y+i*multi +k] == pat[j][i]){
-                        continue;
-                    }
-                    else {
-                        cnt_match--;
-                        match[j]=0;
-                        break;
-                    }
-                }
+    for(int i=N-1;i>=0;i--){
+        int t = Map[i][M*4-1];
+        int cnt=0;
+        for(int j=M*4-1;j>=0;j--){
+            if(Map[i][j]==t){
+                cnt++;
+                count_num[i][j] = {t,cnt};
+            }
+            else{
+                cnt=1;
+                t=Map[i][j];
+                count_num[i][j]={t,cnt};
             }
         }
     }
-    if(cnt_match==0)return -1;
-    for(int i=0;i<match.size();i++){
-        if(match[i])return i;
+}
+int check(int x,int y,int mul){
+    if(count_num[x].size()<y+4){
+        return -1;
     }
+    if (count_num[x][y].first == count_num[x][y + 1].first || count_num[x][y+1].first == count_num[x][y + 2].first||count_num[x][y+2].first==count_num[x][y+3].first){
+        return -1;
+    }
+    vector<int> c;
+    c.push_back(count_num[x][y].second/mul);
+    c.push_back(count_num[x][y+1].second/mul);
+    c.push_back(count_num[x][y+2].second/mul);
+    c.push_back(count_num[x][y+3].second/mul);
+    for(int i=0;i<4;i++){
+        if(c[i]==0)return -1;
+    }
+    for(int i=0;i<10;i++){
+        int f=0;
+        for(int j=0;j<4;j++){
+            if(rat[i][j] !=c[j]){
+                f=1;break;
+            }
+        }
+        if(f==0){
+            return i;
+        }
+    }
+    return -1;
 }
 int main(){
     ios_base::sync_with_stdio(0);
@@ -100,21 +107,23 @@ int main(){
         input();
         int f=0;
         int answer=0;
-        for(int i=0;i<NN-5;i++){
-            for(int j=0;j<M*4-(7*8);j++){
-                //if(sMap[i][j/4]=='0'||sMap[i][j/4]>='8')continue;
-                for(int multi = 1;multi<=4;multi++){
-                    if((j+multi*56) >=(M*4)){
+        for(int i=0;i<N-5;i++){
+            for(int j=0;j<M*4-56;j++){
+                for(int multi = 1;;multi++){
+                    if((j+multi*56>(4*M))){
                         break;
                     }
-                    int val = calc(i,j,multi);
+                    //TODO : check for next start point
+                    int val=check(i,j,multi);
                     if(val==-1)continue;
                     vector<int> pass(1,val);
+                    j--;
                     for(int k=0;k<7;k++){
-                        int temp = calc(i,(j+multi*(k+1)*7),multi);
+                        int temp = check(i,j+(k+1)*4,multi);
                         if(temp==-1)break;
                         else pass.emplace_back(temp);
                     }
+                    j++;
                     if(pass.size()==8){
                         int sum = 0;
                         int sum_1=0;
@@ -127,20 +136,13 @@ int main(){
                         }
                         if((sum_1*3+sum_2+pass[7])%10==0){
                             answer += sum;
-                            for(int a=i;;a++){
-                                if(a==NN)break;
-                                int cnt = 0;
-                                for(int b=j;b<j+multi*56;b++){
-                                    if(Map[a][b]){
-                                        Map[a][b]=0;cnt++;
-                                    }
-                                }
-                                if(cnt==0)break;
-                            }
-                        }              
+                            f=1;
+                            break;
+                        }
                     }
                 }
-            }
+                if(f==1)break;
+            }if(f==1)break;
         }
         cout<<"#"<<tc<<" "<<answer<<endl;
     }
