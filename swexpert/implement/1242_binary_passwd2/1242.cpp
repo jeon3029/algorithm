98d9c34aeae2ca17213c20f290e535eb473624e6
taejoonJeon <taejjeon@gmail.com>
2020-02-15 20:04:55 +0900

    passwd ratio version, but slow

--- swexpert/implement/1242_binary_passwd2/1242.cpp
+++ swexpert/implement/1242_binary_passwd2/1242.cpp
@@ -71,17 +71,27 @@ void input(){
     }
 }
 int check(int x,int y,int mul){
-    if(count_num[x].size()<y+4){
-        return -1;
-    }
-    if (count_num[x][y].first == count_num[x][y + 1].first || count_num[x][y+1].first == count_num[x][y + 2].first||count_num[x][y+2].first==count_num[x][y+3].first){
-        return -1;
-    }
+    int first_number = count_num[x][y].first;
+    int first_count = count_num[x][y].second;
+    y+= first_count;
+    if(first_number==1)return -1;
+    if(y>M*4)return -1;
+    int second_number = count_num[x][y].first;
+    int second_count = count_num[x][y].second;
+    y+= second_count;
+    if(y>M*4)return -1;
+    int third_number = count_num[x][y].first;
+    int third_count = count_num[x][y].second;
+    y+= third_count;
+    if(y>M*4)return -1;
+    int fourth_number = count_num[x][y].first;
+    int fourth_count = count_num[x][y].second;
+    if(first_number==-1 || second_number== -1 ||third_number==-1||fourth_number==-1)return -1;
     vector<int> c;
-    c.push_back(count_num[x][y].second/mul);
-    c.push_back(count_num[x][y+1].second/mul);
-    c.push_back(count_num[x][y+2].second/mul);
-    c.push_back(count_num[x][y+3].second/mul);
+    c.push_back(first_count/mul);
+    c.push_back(second_count/mul);
+    c.push_back(third_count/mul);
+    c.push_back(fourth_count/mul);
     for(int i=0;i<4;i++){
         if(c[i]==0)return -1;
     }
@@ -109,7 +119,8 @@ int main(){
         int answer=0;
         for(int i=0;i<N-5;i++){
             for(int j=0;j<M*4-56;j++){
-                for(int multi = 1;;multi++){
+                if(count_num[i][j].second==-1)continue;
+                for(int multi = 1; multi<=4;multi++){
                     if((j+multi*56>(4*M))){
                         break;
                     }
@@ -117,13 +128,20 @@ int main(){
                     int val=check(i,j,multi);
                     if(val==-1)continue;
                     vector<int> pass(1,val);
-                    j--;
+                    int temp = j;
+                    j+=count_num[i][j].second;
+                    j+=count_num[i][j].second;
+                    j+=count_num[i][j].second;
+                    j+=count_num[i][j].second;
                     for(int k=0;k<7;k++){
-                        int temp = check(i,j+(k+1)*4,multi);
+                        int temp = check(i,j,multi);
                         if(temp==-1)break;
                         else pass.emplace_back(temp);
+                        j+=count_num[i][j].second;
+                        j+=count_num[i][j].second;
+                        j+=count_num[i][j].second;
+                        j+=count_num[i][j].second;
                     }
-                    j++;
                     if(pass.size()==8){
                         int sum = 0;
                         int sum_1=0;
@@ -136,14 +154,27 @@ int main(){
                         }
                         if((sum_1*3+sum_2+pass[7])%10==0){
                             answer += sum;
-                            f=1;
+                            // cout<<i<<","<<temp<<" : "<<sum<<"\n";
+                            for(int a=i;;a++){
+                                int count_one=0;
+                                for(int b=temp;b<temp+multi*56;b++){
+                                    if(count_num[a][b].first == true)count_one++;
+                                    count_num[a][b].first = 0;
+                                    count_num[a][b].second = -1;
+                                }
+                                //cout<<"erase : "<<a<<"\n";
+                                if(count_one==0)break;
+                            }
                             break;
                         }
+                    }
+                    else{
+                        j=temp;
                     }
                 }
                 if(f==1)break;
             }if(f==1)break;
         }
-        cout<<"#"<<tc<<" "<<answer<<endl;
+        cout<<"#"<<tc<<" "<<answer<<"\n";
     }
 }
