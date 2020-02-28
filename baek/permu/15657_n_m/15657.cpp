40e6dceec22cca473fe362cce9f0e98c45b36515
taejoonJeon <taejjeon@gmail.com>
2020-02-28 12:21:43 +0900

    중복 조합

---
+++ baek/permu/15657_n_m/15657.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> Data;
+vector<vector<int>> answer;
+int N,M;
+void dfs(vector<int>current,int len,int pos){
+    if(len==M){
+        answer.push_back(current);
+    }
+    else{
+        for(int i=pos;i<Data.size();i++){
+            current.push_back(Data[i]);
+            dfs(current,len+1,i);
+            current.pop_back();
+        }
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>N>>M;
+    for(int i=0;i<N;i++){
+        int t;cin>>t;
+        Data.push_back(t);
+    }
+    sort(Data.begin(),Data.end());
+    dfs(vector<int>(),0,0);
+    for(int i=0;i<answer.size();i++){
+        for(int j=0;j<answer[i].size();j++){
+            cout<<answer[i][j]<<" ";
+        }cout<<"\n";
+    }
+    return 0;
+}
