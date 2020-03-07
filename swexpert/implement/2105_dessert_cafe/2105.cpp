56bc329e51257a37df12ddb7c03351c5cbaaafe2
taejoonJeon <taejjeon@gmail.com>
2020-03-08 03:17:13 +0900

    최적화

--- swexpert/implement/2105_dessert_cafe/2105.cpp
+++ swexpert/implement/2105_dessert_cafe/2105.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 typedef pair<int,int> pi;
@@ -20,29 +19,38 @@ void input(){
 bool isWall(int x,int y){
     return x<0||y<0||x>=N||y>=N;
 }
-void dfs(int x,int y,int len,int cnt_d,int d){//cd = d count, d = dir
+void dfs(int x,int y,int len,int cnt_d,int d,int r,int c){//cd = d count, d = dir
     if(x==sp.first && y==sp.second && cnt_d==3){
-        if(answer==-1)
-        answer=len;
+        if(answer==-1) answer=len;
         else answer = max(answer,len);
         return;
     }
     if(cnt_d>3)return;
     d--;
-    for(int i=0;i<2;i++){//그대로, 방향 틀기
+    for(int i=0;i<2;i++){//i=0 : 그대로, i=1 : 방향 틀기
         d=(d+1)%4;
         int nx = x+dir[d][0];
         int ny = y+dir[d][1];
+        if(i==1&&cnt_d==2&&len+3-r-c!=r)continue;//직사각형 안되면 방향 틀지 않는다
         if(!isWall(nx,ny)){
             if(!visited[nx][ny] && !used[Data[nx][ny]]){
+                if(i==1){
+                    if(cnt_d ==0){
+                        r = len+1;
+                    }
+                    if(cnt_d==1){
+                        c = len+2-r;
+                        if(answer!=-1 && (len*2)<answer)return;
+                    }
+                }
                 visited[nx][ny]=1;
                 used[Data[nx][ny]]=1;
-                dfs(nx,ny,len+1,cnt_d+i,d);
+                dfs(nx,ny,len+1,cnt_d+i,d,r,c);
                 used[Data[nx][ny]]=0;
                 visited[nx][ny]=0;
             }
             if(nx==sp.first && ny==sp.second && cnt_d+i==3){
-                dfs(nx,ny,len+1,cnt_d+i,d);
+                dfs(nx,ny,len+1,cnt_d+i,d,r,c);
             }
         }    
     }
@@ -55,15 +63,12 @@ int main(){
     for(int tc=1;tc<=T;tc++){
         input();
         answer=-1;
-        for(int i=0;i<N;i++){
+        for(int i=2;i<N;i++){
             for(int j=1;j<N-1;j++){
                 sp.first=i;sp.second=j;
-                for(int d=0;d<4;d++){
-                    memset(used,0,sizeof(used));
-                    memset(visited,0,sizeof(visited));
-                    used[Data[i][j]]=1;
-                    dfs(i,j,0,0,d);
-                }
+                used[Data[i][j]]=1;
+                dfs(i,j,0,0,0,0,0);
+                used[Data[i][j]]=0;
             }
         }
         cout<<"#"<<tc<<" "<<answer<<"\n";
